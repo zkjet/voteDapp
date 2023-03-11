a79df8460f662182a1f6e8ca9e5f437e6a6bb7c7
w3ia <web3ia@gmail.com>
2023-03-11 18:26:13 +0000

    push contracts and backend work

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ethers } from 'ethers';
+import * as tokenJson from './assets/MyToken.json'
+
+const CONTRACT_ADDRESS = "0x3592d257a5fe4111036873754CAF934276C66025";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  provider: ethers.providers.Provider;
+  contract: ethers.Contract;      
+
+  constructor(private config: ConfigService) {
+    this.provider = ethers.getDefaultProvider('sepolia');
+    this.contract = new ethers.Contract(CONTRACT_ADDRESS, tokenJson.abi, this.provider);
+  }
+
+  async getAllowance(from: string, to: string): Promise<number> {
+    return parseInt(ethers.utils.formatEther(await this.contract.allowance(from, to)));
+  }
+
+  async getTotalSupply(): Promise<number> {
+    return parseFloat(ethers.utils.formatEther(await this.contract.totalSupply()));
+  }
+
+  getTokenAddress(): string {
+    const pk = this.config.get<string>('PRIVATE_KEY');
+    console.log(`pk: ${JSON.stringify(pk)}`);
+    return this.contract.address;
+  }
+
+  async giveApproval(to: string, amount: number, signature: string): Promise<boolean> {
+    const pk = this.config.get<string>('PRIVATE_KEY');
+    
+    return await this.contract.approve(to, amount);
   }
 }
+ 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,26 @@
-import { ethers } from "hardhat";
+import { MyToken, MyToken__factory, Ballot__factory } from "./typechain-types";
+import { ethers, Wallet } from 'ethers';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-async function main() {
-    const HelloWorld = await ethers.getContractFactory("HelloWorld");
- 
-    // Start deployment, returning a promise that resolves to a contract object
-    const hello_world = await HelloWorld.deploy();   
-    console.log("Contract deployed to address:", hello_world.address);
- }
- 
- main()
-   .then(() => process.exit(0))
-   .catch(error => {
-     console.error(error);
-     process.exit(1);
-   });
+async function main () {
+    // gets the goerli provider
+    const provider = new ethers.providers.InfuraProvider("sepolia", process.env.INFURA_API_KEY);
+
+    const  pk = process.env.PRIVATE_KEY;
+    if(!pk || pk.length <= 0) throw new Error("Missing environment: private key");
+    const deployerWallet = new ethers.Wallet(pk);
+    console.log(`Connected to deployer's wallet address: ${deployerWallet.address}`);
+
+    const deployer = deployerWallet.connect(provider);
+
+    const contractFactory = new MyToken__factory(deployer);
+    const contract: MyToken = await contractFactory.deploy();
+    const deployTransactionReceipt = await contract.deployTransaction.wait();
+    console.log(`The Tokenized Vote Contract with address ${contract.address}`);
+}
+
+main().catch((error) => {
+    console.log(error);
+    process.exitCode = 1;
+});
