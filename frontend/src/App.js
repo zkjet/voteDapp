4b27c0991b86fd27d43e62caabd5ca2c963302d6
Owen Moore <hello@owenmoore.xyz>
2023-03-11 21:27:50 -0800

    query for a bunch of account information

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,20 +9,32 @@ function App() {
     const [, setSigner] = React.useState(undefined);
 
     return (
-        <div className="App mt-5">
-            <h1 className="p-3">Vote dApp</h1>
-            <Sync setConnected={setConnected} setSigner={setSigner} />
-            {!isConnected ? null : (
-                <>
-                    <Button className="m-1" variant="primary" onClick={() => alert("not implmented")}>
-                        Vote
-                    </Button>
-                    <Button className="m-1" variant="secondary" onClick={() => alert("not implmented")}>
-                        Delegate
-                    </Button>
-                </>
-            )}
-        </div>
+        <>
+            <div className="App mt-5">
+                <h1 className="p-3">Vote dApp</h1>
+                <Sync setConnected={setConnected} setSigner={setSigner} />
+            </div>
+            <div style={{ maxWidth: 960, margin: "auto" }}>
+                {!isConnected ? null : (
+                    <>
+                        <div className="container mb-5">
+                            <h2 className="mb-3">Request Tokens</h2>
+                            <div className="mb-3">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Amount (MTK)"
+                                    aria-label="Amount (MTK)"
+                                />
+                            </div>
+                            <Button className="m-1" variant="primary" onClick={() => alert("not implmented")}>
+                                Submit
+                            </Button>
+                        </div>
+                    </>
+                )}
+            </div>
+        </>
     );
 }
 
--- frontend/src/components/Sync.js
+++ frontend/src/components/Sync.js
@@ -1,18 +1,30 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 
+import axios from "axios";
+import { ethers } from "ethers";
 import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core";
 import { InjectedConnector } from "@web3-react/injected-connector";
+
+import { API_BASE_URL } from "../config/URLs";
 
 const Injected = new InjectedConnector({
     supportedChainIds: [11155111],
 });
 
+const updateInterval = 5000;
+
 export default function Sync({ setConnected, setSigner }) {
     const [errorMessage, setErrorMessage] = React.useState(null);
 
+    const [gasPrice, setGasPrice] = React.useState(null);
+    const [balance, setBalance] = React.useState(null);
+    const [tokenBalance, setTokenBalance] = React.useState(null);
+    const [tokenSupply, setTokenSupply] = React.useState(null);
+
     const { active, account, chainId, error, library, activate } = useWeb3React();
 
+    // Set Error Message (swithc to Sepolia, or unkonwn)
     React.useEffect(() => {
         if (error === undefined) {
             setErrorMessage(null);
@@ -22,20 +34,88 @@ export default function Sync({ setConnected, setSigner }) {
         else setErrorMessage("Error: Unknown");
     }, [error]);
 
+    // Set the isConnected for App
     React.useEffect(() => {
         if (error) setConnected(false);
         setConnected(active);
     }, [active, error, setConnected]);
 
+    // Get the Signer for App
     React.useEffect(() => {
-        if (library !== undefined);
-        setSigner(library?.getSigner());
+        if (!!library) setSigner(library?.getSigner());
     }, [library, setSigner]);
+
+    // Get Account Balanc
+    React.useEffect(() => {
+        if (!!library && account) {
+            library.getBalance(account).then((balanceBN) => {
+                const balanceStr = ethers.utils.formatEther(balanceBN);
+                setBalance(balanceStr);
+            });
+        }
+    }, [account, library]);
+
+    // Get Gas Price (interval)
+    React.useEffect(() => {
+        let gasPriceChecker;
+        if (!!library && account) {
+            library.getBalance(account).then((balanceBN) => {
+                const balanceStr = ethers.utils.formatEther(balanceBN);
+                setBalance(balanceStr);
+            });
+
+            function checkGasPrice() {
+                library.getGasPrice().then((gasPriceBN) => {
+                    const gasPriceStr = ethers.utils.formatEther(gasPriceBN);
+                    setGasPrice(gasPriceStr);
+                });
+            }
+
+            checkGasPrice();
+            gasPriceChecker = setInterval(() => {
+                checkGasPrice();
+            }, updateInterval);
+        }
+        return () => {
+            clearTimeout(gasPriceChecker);
+        };
+    }, [account, library]);
+
+    // Get Account Token Balance (MTK)
+    React.useEffect(() => {
+        if (account) {
+            axios({
+                mathod: "GET",
+                url: `${API_BASE_URL}token/balance-of?address=${account}`,
+            })
+                .then((resp) => {
+                    setTokenBalance(resp.data.balance);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    }, [account]);
+
+    React.useEffect(() => {
+        if (account) {
+            axios({
+                mathod: "GET",
+                url: `${API_BASE_URL}token/supply`,
+            })
+                .then((resp) => {
+                    setTokenSupply(resp.data.supply);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    }, [account]);
 
     return (
         <>
             {errorMessage ? (
-                <div class="alert alert-danger" role="alert">
+                <div className="alert alert-danger" role="alert">
                     {errorMessage}
                 </div>
             ) : null}
@@ -45,13 +125,17 @@ export default function Sync({ setConnected, setSigner }) {
                         activate(Injected);
                     }}
                 >
-                    Sync
+                    Connect
                 </Button>
             ) : (
                 <>
-                    <div class="alert alert-info" role="alert">
+                    <div className="alert alert-info" role="alert">
+                        <div>Network ID: {chainId} (Sepolia)</div>
                         <div>Account: {account}</div>
-                        <div>Network ID: {chainId}</div>
+                        {!gasPrice ? null : <div>Gas Price: {gasPrice} Sepolia ETH</div>}
+                        {!balance ? null : <div>Balance: {balance} Sepolia ETH</div>}
+                        {!tokenBalance ? null : <div>Token Balance: {tokenBalance} MTK</div>}
+                        {!tokenSupply ? null : <div>Total Token Supply: {tokenSupply} MTK</div>}
                     </div>
                 </>
             )}
